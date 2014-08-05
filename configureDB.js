11c199aa733466681788ade91f1c74ea3c6dbf4a
Kalyan Lanka <kalyan.lanka@livingsocial.com>
2014-08-05 16:27:22 -0400

    issue with db connection

--- configureDB.js
+++ configureDB.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/leadgen';
 
-mongoose.connect('mongodb://localhost/leadgen/');
+mongoosed.connect(mongoUri);
 var Schema = mongoose.Schema;
 
 var LeadSchema = new Schema({name: String, 
